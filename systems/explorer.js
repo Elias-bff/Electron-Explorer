6dac70d38ea04b410697c9a7202a4000d89f8365
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-08-08 17:29:19 -0400

    open files, folder recursive, patches, setting stage for custom resizing

--- systems/explorer.js
+++ systems/explorer.js
@@ -1,6 +1,14 @@
 window.onload=function(){explorer.api("init")}
 var sys={
-    ent:function(e,d){e.insertAdjacentHTML("beforeEnd","<p id=\""+(d["parent"]?d["parent"]:"")+d["id"].replace("\\","/")+"\" onclick=\"if(this.children.length){console.log('L')}else{explorer.api('open','"+(d["parent"]?d["parent"]:"")+d["id"].replace("\\","/")+"')}\" class="+d["type"]+">"+d["id"].split("/").at(-1)+"</p>")}}
+    //Add resize functionality
+    class:function(d){
+        switch(d["type"]){
+            case "folder":
+                return "if(this.parentElement.children.length!=1){this.parentElement.replaceChildren(this)}else{explorer.api('view',this.parentElement.id+'/')}"
+            default:
+                return "explorer.api('open','"+(d["parent"]?d["parent"]+"/":"")+d["id"].replace("\\","/")+"')"}},
+    ent:function(e,d){
+        e.insertAdjacentHTML("beforeEnd","<div id=\""+(d["parent"]?d["parent"]:"")+d["id"].replace("\\","/")+"\" class="+d["type"]+"><p onclick=\""+this.class(d)+"\">"+d["id"].split("/").at(-1)+"</p></div>")}}
 explorer.dial("list",(d)=>{
     for(var i=1;i<d.length;i++)
-        sys.ent(document.getElementById(d[0]["folder"].replace("\\","/")),d[i])})
+        sys.ent(document.getElementById(d[0]["folder"].replace("\\","/").slice(0,-1)),d[i])})
--- systems/main.js
+++ systems/main.js
@@ -1,4 +1,4 @@
-const{app,BrowserWindow}=require("electron")
+const{app,BrowserWindow,shell}=require("electron")
 var fs,prc=null
 function popout(bounds,page){
     var win=new BrowserWindow({
@@ -32,20 +32,21 @@ async function main(){
             app.relaunch()
         	app.exit(0)})
         ipcMain.on("init",()=>{prc.exec("fsutil fsinfo drives",(e,s)=>{
-            var drives=s.split(" ").slice(1,-1);var init=[{"folder":"pc"}]
+            var drives=s.split(" ").slice(1,-1);var init=[{"folder":"pc/"}]
             for(var i=0;i<drives.length;i++)
                 init.push({"id":drives[i],"type":"folder"})
             wins[0].webContents.send("list",init)})})
-        ipcMain.on("open",(events,args)=>{
+        ipcMain.on("view",(events,args)=>{
             fs.readdir(args,function(e,r){
                 var f=[{"folder":args}]
                 if(e)return
                 for(var i=0;i<r.length;i++)
-                    f.push({"id":r[i],"parent":args,"type":""})
+                    f.push({"id":r[i],"parent":args,"type":r[i].includes(".")?"file":"folder"})
                 console.log(f)
                 wins[0].webContents.send("list",f)})})
+        ipcMain.on("open",(events,args)=>{shell.openPath(args)})
         wins[0].show()
         wins[0].webContents.executeJavaScript("window.onload()")})}
 app.on("ready",main)
 var sys={
-    callback:function(i,j){BrowserWindow.getAllWindows().find(win=>win.getTitle()==i).webContents.executeJavaScript(j)},}
+    callback:function(i,j){BrowserWindow.getAllWindows().find(win=>win.getTitle()==i).webContents.executeJavaScript(j)}}
