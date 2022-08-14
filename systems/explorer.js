9e277b28480fd26ca23d8509bd95c158fede94b7
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-08-14 13:01:53 -0400

    patch/crude clipboard function

--- systems/explorer.js
+++ systems/explorer.js
@@ -11,7 +11,7 @@ var sys={
     ent:function(e,d){
         try{
             cl=d['type']=="file"?["explorer.api('open','"+(d["parent"]?d["parent"]+"/":"")+d["id"].replace("\\","/")+"')",""]:["if(this.parentElement.children.length!=1){this.parentElement.replaceChildren(this)}else{explorer.api('view',this.parentElement.id+'/')}","<svg width='9' height='9' fill='currentColor' viewBox='0 0 16 16'> <path d='M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z'/></svg>"]
-            e.insertAdjacentHTML("beforeEnd","<div id=\""+(d["parent"]?d["parent"]:"")+d["id"].replace("\\","/")+"\" class="+d["type"]+"><p oncontextmenu=\"explorer.api('inspect',this.parentElement.id)\" onclick=\"\" ondblclick=\""+cl[0]+"\">"+cl[1]+d["id"].split("/").at(-1)+"</p></div>")
+            e.insertAdjacentHTML("beforeEnd","<div id=\""+(d["parent"]?d["parent"]:"")+d["id"].replace("\\","/")+"\" class="+d["type"]+"><p oncontextmenu=\""+(d["type"]=="file"?"explorer.api('inspect',this.parentElement.id)":"")+"\" onclick=\"\" ondblclick=\""+cl[0]+"\">"+cl[1]+d["id"].split("/").at(-1)+"</p></div>")
             }catch{e.insertAdjacentHTML("beforeEnd","<div class='empty'></div>")}}}
 explorer.dial("list",(d)=>{
     var f=document.getElementById(d[0]["folder"].replace("\\","/").slice(0,-1))
